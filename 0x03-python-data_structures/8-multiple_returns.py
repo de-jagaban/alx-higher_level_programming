bca3b452b18f72b51a8310fdc7cb92c96dcb712f
de-jagaban <delightenyinnaya1@gmail.com>
2022-12-05 13:24:54 +0100

    Corrected solution for question 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -7,9 +7,3 @@ def multiple_returns(sentence):
     if sentence == "":
         return (0, None)
     return (len(sentence), sentence[0])
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-
